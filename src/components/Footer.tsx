07bec13bce26823dacb75d7f952a3a38bc86498f
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-04-05 18:57:57 -0300

    feat(layout): improve footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,12 +5,26 @@ const Footer = () => {
     <footer>
       <Container classList={"py-6 border-t border-t-gray-300"}>
         <div className="text-center">
-          <p>
-            &copy;
-            <a target="_blank" href="https://wesleybertipaglia.github.io/">
+          <p className="text-center py-6 text-gray-500">
+            Built with 💙 by{" "}
+            <a
+              target="_blank"
+              href="https://wesleybertipaglia.github.io/"
+              rel="noopener"
+              className="text-blue-500 hover:underline"
+            >
               wesleybertipaglia
-            </a>
-            <span>2025. All rights reserved</span>
+            </a>{" "}
+            for the{" "}
+            <a
+              href="https://dev.to/t/wecoded"
+              target="_blank"
+              rel="noopener"
+              className="text-blue-500 hover:underline"
+            >
+              #wecoded
+            </a>{" "}
+            community on <span className="font-semibold">dev.to</span>
           </p>
         </div>
       </Container>
