312b753f91918dbb5045db8f1375d60ab7e961aa
wesleybertipaglia <wesleybertipaglia@gmail.com>
2025-04-06 08:25:54 -0300

    fix(deploy): base dir

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/wecoded">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
